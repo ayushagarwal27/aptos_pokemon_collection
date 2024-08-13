51240370602adf1857bf830f87ba042d81b4254b
Ayush <ayushagarwal.dev@gmail.com>
2024-08-13 19:06:24 +0530

    add pokemon move contract, frontend integration

---
+++ README.md
@@ -0,0 +1,19 @@
+# APTOS Pokemon Collection APP
+
+Users can login via wallet, for creating pokemon collection, provide name for their collection
+
+Add pokemon to their PokemonCollection resource.
+
+## Smart Contract
+- Move language
+- Struct and entry function for global storage and manipulating the resource
+- View function for getting collection data
+- Test for checking different scenarios
+
+## Frontend
+- React
+- Tailwind for UI
+- Aptos TS SDK for calling smart contract functions
+- Wallet adapter for connecting to wallet
+- Toast Notification
+- Loading State
---
+++ frontend/App.tsx
@@ -0,0 +1,29 @@
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Header } from "@/components/Header";
+import { PokemonCollectionContainer } from "@/components/PokemonCollectionContainer.tsx";
+
+function App() {
+  const { connected } = useWallet();
+
+  return (
+    <>
+      <Header />
+      <div className="flex items-center justify-center flex-col">
+        {connected ? (
+          <Card>
+            <CardContent className="flex flex-col gap-10 pt-6">
+              <PokemonCollectionContainer />
+            </CardContent>
+          </Card>
+        ) : (
+          <CardHeader>
+            <CardTitle className={"text-pink-800 font-bold"}>To get started Connect a wallet</CardTitle>
+          </CardHeader>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
